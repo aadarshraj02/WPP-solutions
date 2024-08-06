3a6caf05ea994be8dc20b8c7c00772582199e0a2
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-06 11:24:40 +0530

    added function to show date and time

--- WPP_day1ans/day1ans.js
+++ WPP_day1ans/day1ans.js
@@ -4,3 +4,30 @@ Sample Output : Today is : Tuesday.
 Current time is : 10 PM : 30 : 38
  */
 
+const showDateAndTime = () => {
+  const now = new Date();
+
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const day = daysOfWeek[now.getDay()];
+
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+  const amPm = hours >= 12 ? "PM" : "AM";
+
+  hours = hours % 12 || 12;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  console.log(`Today is : ${day}.`);
+  console.log(`Current time is : ${hours} ${amPm} : ${minutes} : ${seconds}`);
+};
+showDateAndTime();
