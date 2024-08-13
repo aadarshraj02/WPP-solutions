8c04f44c9cfd7d8486ca24c744933949e307ca09
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-13 18:26:03 +0530

    added program on conditional guessing on number

---
+++ WPP_day3ans/day3ans.js
@@ -0,0 +1,6 @@
+const randomNumber = Math.floor(Math.random() * 10);
+console.log(randomNumber);
+
+const guessedNum = prompt("Guess Number between 1 to 10");
+if (guessedNum === randomNumber) alert("Number Matched");
+else alert("Sorry, Wrong Guess");
