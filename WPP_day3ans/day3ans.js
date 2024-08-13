654cf1f04df14f7e5599be2e01b9828d2a268351
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-13 18:27:37 +0530

    fixed the error(input was string

--- WPP_day3ans/day3ans.js
+++ WPP_day3ans/day3ans.js
@@ -2,5 +2,5 @@ const randomNumber = Math.floor(Math.random() * 10);
 console.log(randomNumber);
 
 const guessedNum = prompt("Guess Number between 1 to 10");
-if (guessedNum === randomNumber) alert("Number Matched");
+if (guessedNum == randomNumber) alert("Number Matched");
 else alert("Sorry, Wrong Guess");
